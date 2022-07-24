d61c9b675739a597e588c8c490596249e2a99100
azkazkazka <sarah_azka@yahoo.com>
2022-07-25 06:28:56 +0700

    feat: add reveal name event

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,10 +1,16 @@
 import {Server} from 'socket.io';
-import {ClientToServerEvents, ServerToClientEvents} from './interface';
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from './interface';
+import roomManager from './roomManager';
+import Room from './revealnameManager';
 
 function socket({
   io,
 }: {
-  io: Server<ClientToServerEvents, ServerToClientEvents>;
+  io: Server<ClientToServerEvents, ServerToClientEvents, never, SocketData>;
 }) {
   console.log('🖥️ Sockets enabled');
 
@@ -23,6 +29,40 @@ function socket({
     // Listen events
     socket.on('hello', () => {
       console.log('Hello from client');
+    });
+
+    socket.on('dummyMatch', roomId => {
+      socket.join(roomId);
+      socket.data.roomId = roomId;
+      const room = roomManager.getRoom(roomId);
+      if (!room) {
+        const newRoom = new Room(roomId);
+        newRoom.setUser(socket.id);
+        roomManager.addRoom(newRoom);
+      } else {
+        room.setUser(socket.id);
+      }
+      console.log(roomManager);
+    });
+
+    socket.on('revealName', () => {
+      const roomId = socket.data.roomId;
+      const room = roomManager.getRoom(roomId);
+      room?.requestReveal(socket.id);
+      if (room?.canRevealName()) {
+        const user1 = room.users[0];
+        const user2 = room.users[1];
+        io.to(roomId).emit('revealName', {
+          user1: `Dummy Name 1 ${user1}`,
+          user2: `Dummy Name 2 ${user2}`,
+        });
+      }
+      console.log(roomManager);
+    });
+
+    socket.on('disconnect', () => {
+      roomManager.deleteRoom(socket.data.roomId);
+      console.log(roomManager);
     });
   });
 }
--- src/socket/interface.ts
+++ src/socket/interface.ts
@@ -2,8 +2,15 @@ export interface ServerToClientEvents {
   noArg: () => void;
   basicEmit: (a: number, b: string) => void;
   withAck: (d: string, callback: (e: number) => void) => void;
+  revealName: (payload: object) => void;
 }
 
 export interface ClientToServerEvents {
   hello: () => void;
+  revealName: () => void;
+  dummyMatch: (roomId: string) => void;
+}
+
+export interface SocketData {
+  roomId: string;
 }
