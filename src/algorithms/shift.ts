4d4f759bdf00940d24e9e46002bc7041538de88c
Altair1618 <farhannabilsuryono3@gmail.com>
2024-05-14 20:25:16 +0700

    docs: add documentation for current algorithms code

--- src/algorithms/shift.ts
+++ src/algorithms/shift.ts
@@ -1,5 +1,12 @@
 import { Block128 } from "../models/Blocks/Block128";
 
+
+/**
+ * Shifts a block to the left
+ * @param {Block128} block
+ * @param {number} shift
+ * @returns {Block128}
+ */
 export function shiftBlock(block: Block128, shift: number): Block128 {
     const size = block.getData().length;
     const data: Uint8Array = new Uint8Array(size);
@@ -11,6 +18,12 @@ export function shiftBlock(block: Block128, shift: number): Block128 {
     return new Block128(data);
 }
 
+/**
+ * Shifts a block to the right
+ * @param {Block128} block
+ * @param {number} shift
+ * @returns {Block128}
+ */
 export function inverseShiftBlock(block: Block128, shift: number): Block128 {
     const size = block.getData().length;
     return shiftBlock(block, size - shift);
--- src/algorithms/substitution.ts
+++ src/algorithms/substitution.ts
@@ -1,6 +1,13 @@
 import { Block128 } from "../models/Blocks/Block128";
 import { INVERSE_S_BOX, S_BOX } from "./constants";
 
+
+/**
+ * Substitutes or inversely substitutes a block using the S-Box or Inverse S-Box
+ * @param {Block128} block
+ * @param {boolean} invert
+ * @returns {Block128}
+ */
 function privateSubstitute(block: Block128, invert: boolean): Block128 {
     const hex: string = block.getHexData();
     const hexArr: string[] = [];
@@ -19,10 +26,20 @@ function privateSubstitute(block: Block128, invert: boolean): Block128 {
     return Block128.fromHex(hexResult);
 }
 
+/**
+ * Substitutes a block using the S-Box
+ * @param {Block128} block
+ * @returns {Block128}
+ */
 export function substitute(block: Block128): Block128 {
     return privateSubstitute(block, false);
 }
 
+/**
+ * Inversely substitutes a block using the Inverse S-Box
+ * @param {Block128} block
+ * @returns {Block128}
+ */
 export function inverseSubstitute(block: Block128): Block128 {
     return privateSubstitute(block, true);
 }
