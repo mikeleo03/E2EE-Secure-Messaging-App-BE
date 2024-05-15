0ebdda3f2252f565a5dc9988e32a15b9ab12ab08
mikeleo03 <leonmichael463@gmail.com>
2024-05-16 01:48:03 +0700

    [Refactor] Creating math utils

--- src/algorithms/ECDH/ECDHUtils.ts
+++ src/algorithms/ECDH/ECDHUtils.ts
@@ -1,48 +1,5 @@
 import { EllipticCurve, ECPoint } from '../ECC/EllipticCurve';
-
-/**
- * Computes the modular reduction.
- * @param {bigint} n - The number to reduce.
- * @param {bigint} p - The modulus.
- * @returns {bigint} The result of n mod p.
- */
-function mod(n: bigint, p: bigint): bigint {
-    return ((n % p) + p) % p;
-}
-
-/**
- * Computes the modular inverse.
- * @param {bigint} n - The number to invert.
- * @param {bigint} p - The modulus.
- * @returns {bigint} The modular inverse of n mod p.
- */
-function inverseMod(n: bigint, p: bigint): bigint {
-    if (n === BigInt(0)) throw new Error('Divide by zero');
-    let [lm, hm] = [BigInt(1), BigInt(0)];
-    let [low, high] = [mod(n, p), p];
-
-    while (low > BigInt(1)) {
-        const ratio = high / low;
-        [lm, hm] = [hm - lm * ratio, lm];
-        [low, high] = [high - low * ratio, low];
-    }
-
-    return mod(lm, p);
-}
-
-/**
- * Generates a random big integer with the specified number of bits.
- * @param {number} bits - The number of bits.
- * @returns {bigint} The generated random big integer.
- */
-function getRandomBigInt(bits: number): bigint {
-    const bytes = Math.ceil(bits / 8);
-    let randomHex = '';
-    for (let i = 0; i < bytes; i++) {
-        randomHex += ('00' + Math.floor(Math.random() * 256).toString(16)).slice(-2);
-    }
-    return BigInt('0x' + randomHex);
-}
+import { mod, inverseMod, getRandomBigInt } from '../Math/MathUtil';
 
 /**
  * Adds two points on the elliptic curve.
