d2857703e4376a4723948eba1207016eeb8153dd
Altair1618 <farhannabilsuryono3@gmail.com>
2024-05-26 10:52:33 +0700

    feat: implement schnorr signature scheme

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,21 +1,30 @@
 import { Request, RequestHandler } from 'express';
-// import { generateBBS } from '../algorithms/BBS/BBS';
-import { decryptMessage, encryptMessage } from '../algorithms/ECC/ECCUtils';
-import { ECPoint } from '../algorithms/ECC/EllipticCurve';
+import SchnorrSignature from '../algorithms/Schnorr/schnorr_signature';
+import { unicodeToHex } from '../utils/string_converter';
 
 interface RequestWithBody<T> extends Request {
   body: T;
 }
 
 const testFunction: RequestHandler = async (
-  req: RequestWithBody<{ plaintext: string; key: string }>,
+  req: RequestWithBody<{ plaintext: string }>,
   res
 ) => {
   try {
-    const { plaintext, key } = req.body;
-    const keyEC: ECPoint = ECPoint.fromString(key);
-    const ciphertext = encryptMessage(plaintext, keyEC);
-    res.status(200).json(ciphertext);
+    const message = req.body.plaintext;
+
+    const keyPair = SchnorrSignature.generateKeyPair();
+    const privateKey = keyPair[0];
+    const publicKey = keyPair[1];
+
+    const hexMessage = "0x" + unicodeToHex(message);
+
+    const signature = SchnorrSignature.generateSchnorrSignature(BigInt(hexMessage), privateKey);
+    const verified = SchnorrSignature.verifySchnorrSignature(BigInt(hexMessage), signature, publicKey);
+
+    const hexSignature = signature.map((x) => x.toString(16));
+
+    res.status(200).json({ hexSignature, verified });
   } catch (error) {
     console.log(error);
     res.status(400).json('Invalid plaintext or key');
